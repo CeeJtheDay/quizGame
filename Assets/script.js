445479efe34dd644b3a66f26f21d3ecde3b07e32
CJ Conrad <carlj.conrad@gmail.com>
2019-10-11 20:45:06 -0700

    more code but doesnt work

--- Assets/script.js
+++ Assets/script.js
@@ -43,55 +43,69 @@
     // printinfo??
 
     // fish quiz
-  
 
+    var Score = 0;
     var qDisplay = $("#queDisplay");
     var aDisplay = $("#ansDisplay");
     var rwDisplay = $("#rwDisplay");
-    var createButton = $("<button>");
+    function createButton(){
+         $("<button>");
+    }
 
     // user arrives at web page
     // presented with iuntroduction and a start button.
     //  clicks start button
-    $("#start").on("click", function startGame(){ 
+    $("#start").on("click", function startGame(){
         var fishQuestions = [
-        {
-            title: "How many kinds of pacific salmon are there??",
-            choices: ["two", "three", "four", "five", "six"],
-            answer: "six"
-        },
-        {
-            title: "true or false: By weight, all of the cephalopods in the ocean weigh more than the entire human poulation",
-            choices: ["true", "false"],
-            answer: "true"
-        },
-        {
-            title: "How long is the average lifespan of an octopus?",
-            choices: ["1-2 years", "4-6 years", "8-10 years", "12-15 years"],
-            answer: "1-2 years"
-        },
-        {
-            title: "Approximately how many species of fish are there worldwide?",
-            choices: ["9,000", "27,000", "56,000", "72,000"],
-            answer: "27,000"
-        },
-        {
-            title: "What is the largest species of fish?",
-            choices: ["Whale Shark", "Giant Gaourami", "Blufin Tuna", "Blue Whale"],
-            answer: "Whale Shark"
-        },
+            {
+              title: "How many kinds of pacific salmon are there??",
+              choices: ["two", "three", "four", "five", "six"],
+              answer: "six"
+            },
+            {
+              title: "true or false: By weight, all of the cephalopods in the ocean weigh more than the entire human poulation",
+              choices: ["true", "false"],
+              answer: "true"
+            },
+            {
+              title: "How long is the average lifespan of an octopus?",
+              choices: ["1-2 years", "4-6 years", "8-10 years", "12-15 years"],
+              answer: "1-2 years"
+            },
+            {
+              title: "Approximately how many species of fish are there worldwide?",
+              choices: ["9,000", "27,000", "56,000", "72,000"],
+              answer: "27,000"
+            },
+            {
+              title: "What is the largest species of fish?",
+              choices: ["Whale Shark", "Giant Gaourami", "Blufin Tuna", "Blue Whale"],
+              answer: "Whae Shark"
+            },
         ];
         aDisplay.empty();
         rwDisplay.empty();
     // presents first question with answers
-    fishQuestions.forEach(function(i = 0, curQuestion){
-        var curQuestion = fishQuestions[i].title;
-        qDisplay.text(curQuestion);
-    
-        
-    })
+      fishQuestions.forEach(function(question){
+          var curQuestion = question.title;
+          var curAnswers = question.choices;
+          var curCorrect = question.answer;
+          qDisplay.text(curQuestion);
+          var ansButton = curAnswers.forEach(createButton());
+          aDisplay.append(ansButton);
+          curAnswers.on("click", function qCounter(){
+            for (i=0; i < fishQuestions.length; i++) {
+                if (curCorrect) {
+                    rwDisplay.text("Correct!");
+                } else {
+                    rwDisplay.text("Incorrect!");
+                }
+            }
+          })
+      });
+
+   
     });
-    
     
     // user chooses answer
     // check if that answer is correct or not
