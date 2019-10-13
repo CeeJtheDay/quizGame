7416e3d3d5a10937cbcf3b236d755c931b301406
CJ Conrad <carlj.conrad@gmail.com>
2019-10-12 23:27:52 -0700

    finally got buttons

--- Assets/script.js
+++ Assets/script.js
@@ -1,31 +1,29 @@
-  $(document).ready()
-
 var fishQuestions = [
-    {
-      title: "How many kinds of pacific salmon are there??",
-      choices: ["two", "three", "four", "five", "six"],
-      answer: "six"
-    },
-    {
-      title: "true or false: By weight, all of the cephalopods in the ocean weigh more than the entire human poulation",
-      choices: ["true", "false"],
-      answer: "true"
-    },
-    {
-      title: "How long is the average lifespan of an octopus?",
-      choices: ["1-2 years", "4-6 years", "8-10 years", "12-15 years"],
-      answer: "1-2 years"
-    },
-    {
-      title: "Approximately how many species of fish are there worldwide?",
-      choices: ["9,000", "27,000", "56,000", "72,000"],
-      answer: "27,000"
-    },
-    {
-      title: "What is the largest species of fish?",
-      choices: ["Whale Shark", "Giant Gaourami", "Blufin Tuna", "Blue Whale"],
-      answer: "Whae Shark"
-    },
+  {
+    title: "How many kinds of pacific salmon are there??",
+    choices: ["two", "three", "four", "five", "six"],
+    answer: "six"
+  },
+  {
+    title: "true or false: By weight, all of the cephalopods in the ocean weigh more than the entire human poulation",
+    choices: ["true", "false"],
+    answer: "true"
+  },
+  {
+    title: "How long is the average lifespan of an octopus?",
+    choices: ["1-2 years", "4-6 years", "8-10 years", "12-15 years"],
+    answer: "1-2 years"
+  },
+  {
+    title: "Approximately how many species of fish are there worldwide?",
+    choices: ["9,000", "27,000", "56,000", "72,000"],
+    answer: "27,000"
+  },
+  {
+    title: "What is the largest species of fish?",
+    choices: ["Whale Shark", "Giant Gaourami", "Blufin Tuna", "Blue Whale"],
+    answer: "Whae Shark"
+  },
 ];
 
 var qIndex = 0;
@@ -37,9 +35,6 @@ var aDisplay = $("#ansDisplay");
 var rwDisplay = $("#rwDisplay");
 var timer = $("#timer");
 
-function createButton(){
-  $("<button>");
-};
 
 function startGame() {
   displayQuestion();
@@ -47,63 +42,82 @@ function startGame() {
 };
 
 function setTime() {
-  var timerInterval = setInterval(function() {
+  var timerInterval = setInterval(function () {
     secondsLeft--;
     var startTime = $("#timer").html();
     timer.html(secondsLeft);
 
-    if(secondsLeft === 0) {
+    if (secondsLeft === 0) {
       clearInterval(timerInterval);
-      sendMessage();
     }
 
   }, 1250)
 };
 
-function displayQuestion (){
+function displayQuestion() {
   var curQuestion = questions.title;
   //presents question to user
   qDisplay.text(curQuestion);
 };
 //presents answers to user
-    // funtion to diplay answer buttons
-function displayAnswers(){
+// funtion to diplay answer buttons
+function displayAnswers() {
   var curAnswers = questions.choices;
-  curAnswers.forEach(createButton()); 
+  $.each(curAnswers, function(i, answer){
+    aDisplay.append("<button>" + answer + "</button");
     $("button").addClass("answer-button");
-    aDisplay.append($("button"));
-  }
-    // -- Check Answer --
-    $(".answer-button").on("click", function(){
-      for (j = 0; j < questions.choices.length; j++) {
-       var curAnswer = questions.choices[j];
-         
-       if (curAnswer === questions.answer) {
-         score + 5;
-         rwDisplay.text("Correct!");
-       } else {
-         score - 5;
-         secondsLeft - 15;
-         rwDisplay.text("Nope!");
-       }
-       qIndex++;
+  })
+  $(".answer-button").on("click", function () {
+    // for (j = 0; j < questions.choices.length; j++) {
+      Answer = questions.answer;
+      if (event === answer) {
+        score + 5;
+        rwDisplay.text("Correct!");
+      } else {
+        score - 5;
+        secondsLeft - 15;
+        rwDisplay.text("Nope!");
       }
-    });
+      qIndex++;
+    })
+};
+  // for (i = 0; i < curAnswers, length; i++) {
+  //   var buttons = $("<button>").text(curAnswers[i]);
+  //   $("button").addClass("answer-button");
+  //   buttons.append($(aDisplay));
+  // }};
+
+// -- Check Answer --
+// $(".answer-button").on("click", function () {
+//   for (j = 0; j < questions.choices.length; j++) {
+//     var curAnswer = questions.choices[j];
+//     if (curAnswer === questions.answer) {
+//       score + 5;
+//       rwDisplay.text("Correct!");
+//     } else {
+//       score - 5;
+//       secondsLeft - 15;
+//       rwDisplay.text("Nope!");
+//     }
+//     qIndex++;
+//   }
+// });
 
 
 
- // ---- Start game ---
- $("#start").on("click", function () {
+
+// ---- Start game ---
+$("#start").on("click", function () {
   qDisplay.empty();
   aDisplay.empty();
   rwDisplay.empty();
   setTime();
   startGame();
-// 1.start Timer
-  
+  // 1.start Timer
+
   // -- Cycle Questions ---
-//     // 2.pick a card (loop)
-  });
+  //     // 2.pick a card (loop)
+});
 
     // -- Read question & choices --
         // 3.read the question
